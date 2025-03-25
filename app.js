77424a0644c86c9e236d538dac46648bfa453e28
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-03-25 16:09:50 +0530

    sending a resposne from http server

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const http = require('http');
 // http.createServer(function (req, res) {});
 
 const server = http.createServer((req, res) => {
-  console.log('req', req);
+  // console.log('req', req.url, req.method, req.headers);
   // process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>NodeJS HTTP Server</title></head>');
+  res.write('<body><h1>NodeJs http server response...</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000, () => {});
