51d674bf5dfb761f226dfb0b34d2c0f52894d4e0
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-03-26 01:35:04 +0530

    getting data from request with node event listeners

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // function reqListener(req, res) {}
 // http.createServer(reqListener);
@@ -7,13 +8,44 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   // console.log('req', req.url, req.method, req.headers);
-  // process.exit();
+
+  const { url, method } = req;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</buttton></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log('message', message);
+    });
+
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>NodeJS HTTP Server</title></head>');
-  res.write('<body><h1>NodeJs http server response...</h1></body>');
+  res.write('<body><h 1>NodeJs http server response...</h1></body>');
   res.write('</html>');
   res.end();
+
+  // process.exit();
 });
 
 server.listen(3000, () => {});
