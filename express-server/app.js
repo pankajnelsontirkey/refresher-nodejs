201d59633a52918f6cfa7c2a5cf2d8d7d0d20683
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-03-28 20:29:22 +0530

    refactored to use pug template engine

--- express-server/app.js
+++ express-server/app.js
@@ -2,26 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const { engine } = require('express-handlebars');
 
-const { router: adminRoutes } = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const { adminRoutes } = require('./routes/admin');
+const { shopRoutes } = require('./routes/shop');
 
 const app = express();
 
-// app.engine(
-//   'hbs',
-//   engine({
-//     layoutsDir: 'views/hbs/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-//   })
-// );
-
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
-app.set('view engine', 'ejs');
-
+app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,7 +18,6 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   res.status(404).render('404', {
     pageTitle: 'Page not found',
     layout: 'main-layout',
