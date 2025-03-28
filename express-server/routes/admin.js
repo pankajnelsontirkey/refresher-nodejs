f005c601ecf6afd07e45cb4292a9b2989cf9745c
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-03-28 15:22:09 +0530

    added views using handlebars engine

--- express-server/routes/admin.js
+++ express-server/routes/admin.js
@@ -11,7 +11,10 @@ router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
   res.render('add-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    activeAddProduct: true,
+    formsCSS: true,
+    productCSS: true
   });
 });
 
--- express-server/routes/shop.js
+++ express-server/routes/shop.js
@@ -9,7 +9,15 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   // console.log(products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop', { products, pageTitle: 'Shop', path: '/' });
+  res.render('shop', {
+    products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+    // layout: false
+  });
 });
 
 module.exports = router;
