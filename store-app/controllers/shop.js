c8556640bc8cb561514b3ca08468d3e447a297a8
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-07-22 20:20:24 +0530

    added getInvoice controller;

--- store-app/controllers/shop.js
+++ store-app/controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -160,4 +163,18 @@ exports.getOrders = (req, res) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const { orderId } = req.params;
+  const invoiceName = `invoice_${orderId}.pdf`;
+
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.send(data);
+  });
 };
